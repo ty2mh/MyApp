8dbf8ebe8e55c685285bce23f5f0bc423c61c86b
ty2mh <leety1930@kakao.com>
2022-03-16 17:11:07 +0900

    login

--- index.js
+++ index.js
@@ -4,3 +4,4 @@
 var mydom = document.getElementById('myid');
 console.log(mydom);
 console.log('댓글기능 추가 로그 출력');
+console.log('로그인기능 추가 로그 출력');
--- login.js
+++ login.js
@@ -1,2 +1,3 @@
 //login 
 console.log('loginjs');
+console.log('로그인 기능 로그 출력');
